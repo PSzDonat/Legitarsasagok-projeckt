9158e9a9fc83c39228d56a320a9cec47ee2e7b76
garobcsi <gasparrobert0908@gmail.com>
2023-01-23 11:54:10 +0100

    Reconfigure asp.net Rest Api

--- Legitarsasagok-REST-API/Legitarsasagok-REST-API/Startup.cs
+++ Legitarsasagok-REST-API/Legitarsasagok-REST-API/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,8 +42,6 @@ namespace Legitarsasagok_REST_API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Legitarsasagok_REST_API v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
